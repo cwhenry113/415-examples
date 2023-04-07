6a60cbb9f1b1c2f39bc73e08535e3e4318f708f2
cwhenry113 <carter.henry@selu.edu>
2023-04-07 12:52:39 -0500

    Project Phase 1

--- server.js
+++ server.js
@@ -1,18 +1,61 @@
-var express = require('express');
-var chalk = require('chalk');
-var app = express();
-var router = express.Router();
+const express = require('express');
+const app = express();
+const router = express.Router();
+const fs = require('fs');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-router.get('/test', function(req, res) {
-    res.status(200).send('Hello world');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+app.use('/', router);
+
+app.listen(3000);
+console.log("Server started");
+
+//Find and display all tickets
+router.get('/rest/list', function(req, res) {
+    fs.readdir("./tickets", (err, ticketIDs) => {
+        const tickets = new Array();
+        ticketIDs.forEach(fileID => {
+            fs.readFile("./tickets/" + fileID, "utf8", (err, jsonString) => {
+                tickets.push(jsonString);
+                if(tickets.length == ticketIDs.length){
+                    res.send(tickets);
+                }
+            });
+        })
+    });
 });
 
-app.use('/api', router);
 
-app.listen(1069, function(err) {
-    if (err) {
-        console.log(chalk.red(err));
-    } else {
-        console.log(chalk.blue('Magic Happens on Port 69'));
-    }
+//Find and display a ticket with given id
+router.get('/rest/ticket/id', function(req, res) {
+    console.log(req.body);
+    fs.readFile("./tickets/" + req.body.id + ".json", "utf8", (err, jsonString) => {
+        if (err){
+            res.status(200).send("ID not found");
+            return;
+        }
+        res.status(200).send(jsonString);
+    });
 });
+
+//Create a new ticket
+router.post('/rest/ticket', function(req, res) {
+    const newTicket = {
+        id: req.body.id,
+        type: req.body.type,
+        subject: req.body.subject,
+        description: req.body.description,
+        status: req.body.status
+    };
+    const newTicketString = JSON.stringify(newTicket);
+    fs.writeFile("./tickets/" + req.body.id + ".json", newTicketString, err => {
+        if (err) {
+            res.status(200).send("Error writing ticket, ticket not recorded.")
+        } else {
+            res.status(200).send("Ticket successfully recorded")
+        }
+    });
+});
+
